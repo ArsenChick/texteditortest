4b0ae9f4e2b1b361c87e5a2233f9d24325d7c32e
ArsenChick <arseniy1304@gmail.com>
2020-04-29 12:23:27 +0300

    fix for editor.cpp

--- app/editor.cpp
+++ app/editor.cpp
@@ -9,6 +9,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <cassert>
 
 #include "text/text.h"
 #include "common.h"
@@ -29,7 +30,8 @@ int main()
         printf("ed > ");
         
         /* Получаем команду */
-        fgets(cmdline, MAXLINE, stdin);
+        char* success = fgets(cmdline, MAXLINE, stdin);
+        assert(success != NULL);
 
         /* Извлекаем имя команды */
         if ((cmd = strtok(cmdline, " \n")) == NULL) {
